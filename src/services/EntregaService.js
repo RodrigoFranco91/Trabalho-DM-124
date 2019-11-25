2459d17fd763f661f66f5db87043223a8cce001c
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2019-11-25 12:09:11 -0300

    Criando a classe EntregaService

---
+++ src/services/EntregaService.js
@@ -0,0 +1,29 @@
+const db = {};
+
+let sequence = 0;
+
+class EntregaService {
+
+  static add(newEntrega) {
+
+  }
+
+  static getAll() {
+
+  }
+
+  static getById(id) {
+
+  }
+
+  static update(id, updatedEntrega) {
+    
+  }
+
+  static delete(id) {
+
+  }
+  
+}
+
+module.exports = EntregaService;
