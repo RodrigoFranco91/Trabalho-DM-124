a43866e52d51e0bd02fabac7b156b48c54b47db3
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2019-11-25 12:12:09 -0300

    Importando EntregaService em routes/entregas.js

--- src/routes/entregas.js
+++ src/routes/entregas.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const entregaService = require('../services/EntregaService');
 
 router.post('/', checkAuth, async (request, response) => {
 
