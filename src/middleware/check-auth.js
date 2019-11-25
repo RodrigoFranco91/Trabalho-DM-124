42683c5767f77f8bf8a25a65e4fa401bfbb28732
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2019-11-25 12:38:39 -0300

    Criando o middleware check-auth

---
+++ src/middleware/check-auth.js
@@ -0,0 +1,18 @@
+// Utilizando Basic Auth
+// Username = inatel
+// Password = alunosinatel
+
+const checkAuth = (request, response, next) => {
+    const token = request.headers.authorization;
+    if (/aW5hdGVsOmFsdW5vc2luYXRlbA==/.test(token)) {
+        aW5hdGVsOmFsdW5vc2luYXRlbA==
+      next();
+    } else {
+      response.status(401).json({
+        message: 'Você deve estar logado!',
+        status: 401
+      });
+    }
+  };
+  
+  module.exports = checkAuth;
